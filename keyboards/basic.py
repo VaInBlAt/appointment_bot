e03f41069ef95f5deb201d67aa2ce1b4db76a192
VainblatS <133905420+VaInBlAt@users.noreply.github.com>
2025-11-18 21:05:02 +0300

    v.0.1

---
+++ keyboards/basic.py
@@ -0,0 +1,54 @@
+from base import KeyboardBuilder
+from aiogram.types import InlineKeyboardMarkup
+
+class MainMenu:
+    @staticmethod
+    def start() -> InlineKeyboardMarkup:
+        return KeyboardBuilder.inline(
+            buttons={
+                'Регистрация': 'registration_step1_-'
+            },
+            row_widths=[1])
+    
+    @staticmethod
+    def step1() -> InlineKeyboardMarkup:
+        return KeyboardBuilder.inline(
+            buttons={
+                'Я - врач': 'registration_step2_doctor',
+                'Я - Пациент': 'registration_step2_patient'
+            },
+            row_widths=[1])
+    
+    @staticmethod
+    def skip_step() -> InlineKeyboardMarkup:
+        return KeyboardBuilder.inline(
+            buttons={
+                'Пропустить': 'registration_skip_-'
+            },
+            row_widths=[1])
+    
+    @staticmethod
+    def confirm_registration() -> InlineKeyboardMarkup:
+        return KeyboardBuilder.inline(
+            buttons={
+                '✅ Подтвердить': 'registration_confirm_-',
+                '🔄 Заполнить заново': 'registration_restart_-'
+            },
+            row_widths=[1])
+
+    @staticmethod
+    def main_menu() -> InlineKeyboardMarkup:
+        return KeyboardBuilder.inline(
+            buttons={
+                '📊 Личный кабинет': 'profile',
+                '📅 Записаться на прием': 'appointment_calendar'
+            },
+            row_widths=[1])
+    
+    @staticmethod
+    def exit() -> InlineKeyboardMarkup:
+        return KeyboardBuilder.inline(
+            buttons={
+                '🏠 На главную': 'exit'
+            },
+            row_widths=[1])
